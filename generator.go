06f264e940af0510318ec80b8c8ff238dedcba05
Ryo Takashima <r.takashima@miniascape.co.jp>
2018-02-22 20:32:18 +0900

    generator

--- generator.go
+++ generator.go
@@ -1,4 +1,21 @@
 package main
 
+import "fmt"
+
+func integers() func() int {
+	i := 0
+	return func() int {
+		i += 1
+		return i
+	}
+}
+
 func main() {
+	ints := integers()
+	fmt.Println(ints())
+	fmt.Println(ints())
+	fmt.Println(ints())
+
+	other := integers()
+	fmt.Println(other())
 }
